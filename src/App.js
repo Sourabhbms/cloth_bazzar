e4aafecfbc7bdcf148f91efe56dc4e77bdab1da9
Sourabh Mukherjee <sourabhbms@gmail.com>
2023-01-04 23:37:59 +0530

    Sign In

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import {Routes, Route} from 'react-router-dom';
 import Navigation from "./routes/navigation/navigation.component";
-
+import SignIn from "./routes/sign-in/sign-in.component";
 
 
 
@@ -15,9 +15,9 @@ const App = () => {
   <Routes>
     <Route path='/' element={<Navigation />}>
 
-      <Route index={true} element={<Home />} /> 
+      <Route index element={<Home />} /> 
       <Route path="shop" element={<Shop />} />
-
+      <Route path="sign-in" element={<SignIn />} />
     </Route>
     
   </Routes>
