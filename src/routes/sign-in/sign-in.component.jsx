289e2df23e789d862c18f03ad9ae18ca00283cb2
Sourabh Mukherjee <sourabhbms@gmail.com>
2023-01-06 22:38:14 +0530

    Google Redicrect Async

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,36 @@
-import { signInWithGooglePopup,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { auth,signInWithGooglePopup,createUserDocumentFromAuth,signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 
 const SignIn = () => {
+
+    useEffect( ()=>{
+        async function fetchGoogleRedirect() {
+            const response =  await getRedirectResult(auth);
+            if(response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+        
+        fetchGoogleRedirect();
+
+    }, []);
+
     const logGoogleUser=async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
+
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign In With Google Popup
+            </button>
+
+            <button onClick={signInWithGoogleRedirect}>
+                Sign In With Google Redirect
             </button>
         </div>
     )
