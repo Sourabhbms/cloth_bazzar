7e1ad332d31d67dad80e95dd3539991999427ec9
Sourabh Mukherjee <sourabhbms@gmail.com>
2023-01-08 17:49:36 +0530

    Sign In /Sign Out

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,29 +1,41 @@
-import { Fragment } from 'react';
-import {Outlet, Link} from 'react-router-dom';
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { UserContext } from '../../contexts/user.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
 import './navigation.styles.scss';
 
-const Navigation = ()=> {
-    return (
-      <Fragment>
-        <div className='navigation'>
-            <Link className='logo-container' to='/'>
-                <CrwnLogo className='logo'/>
-            </Link>
-          
-          <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>
-                SHOP
-            </Link>
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
 
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
             <Link className='nav-link' to='/auth'>
-                SIGN IN
+              SIGN IN
             </Link>
-          </div>
+          )}
         </div>
-        <Outlet/>
-      </Fragment>
-    )
-  }
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
-  export default Navigation;
+export default Navigation;
